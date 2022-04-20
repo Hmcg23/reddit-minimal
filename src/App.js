2020ff2a69a3918dba5f9a0667040445b321583b
H_mcg <82006254+Hmcg23@users.noreply.github.com>
2022-04-20 16:59:27 -0700

    slight css changes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import redditlogo from './images/redditlogo.png';
 import searchicon from './images/searchicon.svg';
 import messageicon from './images/messageicon.svg'
-import arrow from './images/arrow.svg'
-import { Counter } from './features/counter/Counter';
+import arrow from './images/arrow.svg';
+import bgimage from './images/bgimage.jpeg'
+// import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      {/* Nav Bar */}
       <nav className="nav-bar">
         <div className="left-side">
           <img src={redditlogo} alt="reddit logo" className="reddit-logo" />
@@ -20,7 +21,7 @@ function App() {
           <h2 className="text">Search</h2>     
         </div>
       </nav>
-
+      {/* All Posts*/}
       <body>
         <main>
         <div className="all-posts">
@@ -33,7 +34,7 @@ function App() {
 
           <div className="post-info">
             <div className="post-text">
-              <h2>Top 10 greatest <a href="https://scratch.mit.edu/projects/677407763/" target="_blank">scratch intros</a></h2>
+              <h2>Top 10 greatest <a href="https://scratch.mit.edu/projects/677407763/" target="_blank" rel="noreferrer">scratch intros</a></h2>
               <hr />            
             </div>
 
@@ -46,10 +47,34 @@ function App() {
               </div>
             </div>          
           </div>
-          </section>          
+          {/* Individual Post */}
+          </section>
+          <section className="post">
+            <div className="upvote-system">
+              <img src={arrow} alt="upvote-arrow" className="arrow" />
+              <b>12.2k</b>
+              <img src={arrow} alt="upvote-arrow" className="arrow" />
+            </div>
+
+          <div className="post-info">
+            <div className="post-text">
+              <h2>Cool Photo</h2>
+              <img src={bgimage} alt="post" className="post-image" />
+              <hr />   
+            </div>
+
+            <div className="user-info">
+              <p>ilovemydog</p>
+              <p>1 month ago</p>
+              <div className="messages">
+                <img src={messageicon} alt="message icon" className="message-icon" />
+                <p>244 comments</p>   
+              </div>
+            </div>          
+          </div>
+          </section>
         </div>
-
-
+        {/* Subreddit  */}
           <section className="subreddit-section">
             <h1>Subreddits</h1>
             <div className="subreddit">
